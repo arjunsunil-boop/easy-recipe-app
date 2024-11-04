706f67dfea62c7f90c1b2b6d0b2cd9cc087df5ec
Angelishere <angelsara1804@gmail.com>
2024-11-04 22:37:18 +0530

    Add recipe and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddRecipes from './components/AddRecipes';
 
 function App() {
   return (
     <div>
-
+      <AddRecipes />
     </div>
   );
 }
